b4f92b4a809d5d33dd92be0cb4823f7dc3be3bfe
Samuel Yeung <samuelyeung.hk@gmail.com>
2019-09-01 23:22:22 -0400

    1. now press tab when searching would auto advance to until end of word, shift+tab for opposite 2. revsere search direction

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ module.exports.LEAD_KEY = process.platform === 'darwin' ? 'Cmd' : 'Shift+Ctrl';
 module.exports.EDIT = 'Edit';
 module.exports.ENTER = 'Enter';
 module.exports.ESCAPE = 'Escape';
+module.exports.TAB = 'Tab';
 module.exports.DIRECTION_NEXT = 1;
 module.exports.DIRECTION_PREV = -1;
 module.exports.STYLE_CLASSES = {
